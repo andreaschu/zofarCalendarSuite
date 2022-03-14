963f30955a44b706c3a86ec6938058c4cb21d9f2
christian-fr <friedrich@dzhw.eu>
2022-03-14 18:40:07 +0100

    first implementation of fragmentMemoryCalculator.py

--- fragmentMemoryCalculator/fragmentMemoryCalculator.py
+++ fragmentMemoryCalculator/fragmentMemoryCalculator.py
@@ -1,8 +1,14 @@
 # pip install python-snappy
+from abc import ABC
+
 import snappy
 import random
 import string
 from collections import defaultdict
+import lorem
+from dataclasses import dataclass
+import abc
+import math
 
 
 def hexdecode_and_decompress(input_str: str) -> str:
@@ -11,12 +17,11 @@ def hexdecode_and_decompress(input_str: str) -> str:
     return decompressed_bytes.decode('utf-8')
 
 
-s = 'bc02685b7b226964223a302c227374617465223a226e6577222c22747970010d10536c6f74320d0f2c436f6c6f72223a2272656422052f08727444093374323032302d30332d30315430313a30303a30302e3030305a222c22656e64192518332d30322d32384225004473665f7661613031223a66616c73657d2c7b111300350d13002c05a600310577fea600fea60011a6005d'
-b = bytes.fromhex(s)
-
-t = snappy.decompress(b)
-r = t.decode('utf-8')
-print(r)
+def return_lorem_text(length: int = 2000):
+    para1 = ''
+    while len(para1) < length:
+        para1 += next(lorem.paragraph(count=1, comma=(0, 2), word_range=(12, 13), sentence_range=(30, 35))) + ' '
+    return para1[:1999] + '.'
 
 
 def create_test_dict(number_of_keys: int, val_length: int) -> dict:
@@ -24,11 +29,11 @@ def create_test_dict(number_of_keys: int, val_length: int) -> dict:
     for i in range(number_of_keys):
         max_str_length_i = len(str(number_of_keys))
         key = f'key' + str(i).zfill(max_str_length_i)
-        tmp_dict[key] = random_string_generator(val_length)
+        tmp_dict[key] = return_random_string(val_length)
     return tmp_dict
 
 
-def random_string_generator(length: int) -> string:
+def return_random_string(length: int) -> string:
     all_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + ' []{}()_-'
     tmp_str = ''
     for i in range(length):
@@ -45,6 +50,246 @@ def hexencode_and_compress(input_str: str) -> str:
 def compress(input_str: str) -> str:
     return snappy.compress(input_str)
 
+
+class FormalZofarQuestionInterface(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'return_random_ao_json_str') and
+                callable(subclass.return_random_ao_json_str))
+
+
+@dataclass
+@FormalZofarQuestionInterface.register
+class ZofarQuestion:
+    """Superclass for simulating zofar question"""
+    list_of_variable_names: list
+
+    def return_random_ao_json_str(self):
+        raise NotImplementedError()
+
+    def return_details(self):
+        raise NotImplementedError()
+
+
+@dataclass
+class ZofarQuestionSingleChoice(ZofarQuestion, ABC):
+    no_of_answer_options: int
+    question_type: str = 'questionSingleChoice'
+    list_of_answer_options: list = None
+
+    def set_up(self):
+        # check interface
+        assert issubclass(self.__class__, FormalZofarQuestionInterface)
+
+        # create list of answer options
+        self.list_of_answer_options = ['ao' + str(i) for i in range(self.no_of_answer_options)]
+        # check if exactly one variable name is given
+        assert len(self.list_of_variable_names) == 1
+        # check if varname is a str
+        assert isinstance(self.list_of_variable_names[0], str)
+        # assert that varname is str of length > 0
+        assert len(self.list_of_variable_names[0]) > 0
+
+    def return_random_ao_json_str(self):
+        self.set_up()
+        ao = random.choice(self.list_of_answer_options)
+        varname = self.list_of_variable_names[0]
+        return f'"{varname}": "{ao}"'
+
+    def return_details(self) -> str:
+        output_str = f'{self.question_type=}, {self.list_of_variable_names=}, {self.no_of_answer_options=}'
+        return output_str
+
+@dataclass
+class ZofarQuestionSingleChoiceMatrix(ZofarQuestion, ABC):
+    no_of_answer_options: int
+    question_type = 'questionSingleChoiceMatrix'
+    list_of_answer_options: list = None
+    list_of_sc_question_objects: list = None
+
+    def set_up(self):
+        # clear self.list_of_sc_question_objects
+        self.list_of_sc_question_objects = []
+        # check interface
+        assert issubclass(self.__class__, FormalZofarQuestionInterface)
+
+        # create list of answer options
+        self.list_of_answer_options = ['ao' + str(i) for i in range(self.no_of_answer_options)]
+        # check if exactly one variable name is given
+        assert len(self.list_of_variable_names) > 0
+        # iterate over all varnames
+        for varname in self.list_of_variable_names:
+            try:
+                # check if varname is a str
+                assert isinstance(varname, str)
+                # assert that varname is str of length > 0
+                assert len(varname) > 0
+            except AssertionError as e:
+                print(f'{varname=}')
+                print(f'{e=}')
+            self.list_of_sc_question_objects.append(
+                ZofarQuestionSingleChoice(no_of_answer_options=self.no_of_answer_options,
+                                          list_of_variable_names=[varname]))
+
+    def return_random_ao_json_str(self):
+        self.set_up()
+        list_of_json_strings = []
+        for question_object in self.list_of_sc_question_objects:
+            list_of_json_strings.append(question_object.return_random_ao_json_str())
+        return ', '.join(list_of_json_strings)
+
+    def return_details(self) -> str:
+        output_str = f'{self.question_type=}, {self.list_of_variable_names=}, {self.no_of_answer_options=}'
+        return output_str
+
+
+@dataclass
+class ZofarQuestionMultipleChoice(ZofarQuestion, ABC):
+    question_type: str = 'questionMultipleChoice'
+    list_of_answer_options: tuple = tuple(['True', 'False'])
+
+    def return_random_ao_json_str(self):
+        list_of_json_strings = []
+        for varname in self.list_of_variable_names:
+            value = random.choice(self.list_of_answer_options)
+            list_of_json_strings.append(f'"{varname}": "{value}"')
+        return ', '.join(list_of_json_strings)
+
+    def return_details(self) -> str:
+        output_str = f'{self.question_type=}, {self.list_of_variable_names=}, {self.list_of_answer_options=}'
+        return output_str
+
+
+@dataclass
+class ZofarQuestionMultipleChoiceMatrix(ZofarQuestion, ABC):
+    no_of_answer_options: int
+    question_type = 'questionMultipleChoiceMatrix'
+
+    list_of_indexed_variable_names: list = None
+    list_of_mc_question_objects: list = None
+
+    def set_up(self):
+        # clear self.list_of_mc_question_objects
+        self.list_of_mc_question_objects = []
+        # check interface
+        assert issubclass(self.__class__, FormalZofarQuestionInterface)
+
+        # create list of items
+        self.list_of_indexed_variable_names = [f'{j}_{k}' for j in self.list_of_variable_names for k in
+                                               range(self.no_of_answer_options)]
+        # check if exactly one variable name is given
+        assert len(self.list_of_variable_names) > 0
+        # iterate over all varnames
+        for varname in self.list_of_indexed_variable_names:
+            try:
+                # check if varname is a str
+                assert isinstance(varname, str)
+                # assert that varname is str of length > 0
+                assert len(varname) > 0
+            except AssertionError as e:
+                print(f'{varname=}')
+                print(f'{e=}')
+            self.list_of_mc_question_objects.append(
+                ZofarQuestionMultipleChoice(list_of_variable_names=[varname]))
+
+    def return_random_ao_json_str(self):
+        self.set_up()
+        list_of_json_strings = []
+        for question_object in self.list_of_mc_question_objects:
+            list_of_json_strings.append(question_object.return_random_ao_json_str())
+        return ', '.join(list_of_json_strings)
+
+    def return_details(self) -> str:
+        output_str = f'{self.question_type=}, {self.list_of_variable_names=}, {self.no_of_answer_options=}'
+        return output_str
+
+
+@dataclass
+class ZofarQuestionOpen(ZofarQuestion, ABC):
+    question_type: str = 'questionOpen'
+    length_of_text: int = 2000
+
+    def return_random_ao_json_str(self):
+        varname = self.list_of_variable_names[0]
+
+        return f'"{varname}": "{return_random_string(self.length_of_text)}"'
+
+    def return_details(self) -> str:
+        output_str = f'{self.question_type=}, {self.list_of_variable_names=}, {self.length_of_text=}'
+        return output_str
+
+
+class ZofarQuestionCollection:
+    def __init__(self, numer_of_episodes: int = 1, chars_per_db_variable: int = 1500):
+        self.list_of_zofar_questions = []
+        self.number_of_episodes = numer_of_episodes
+        self.chars_per_db_variable = chars_per_db_variable
+
+    def add_question_object(self, question_object: ZofarQuestion):
+        self.list_of_zofar_questions.append(question_object)
+
+    def return_random_ao_json_str(self):
+        output_list_of_jsons = []
+        for i in range(self.number_of_episodes):
+            list_of_random_json = []
+            for question in self.list_of_zofar_questions:
+                list_of_random_json.append(question.return_random_ao_json_str())
+            output_list_of_jsons.append(', '.join(list_of_random_json))
+        return '{[' + '], ['.join(output_list_of_jsons) + ']}'
+
+    def print_statistics(self):
+        print(f'{self.number_of_episodes=}')
+        tmp_details_list = [question_object.return_details() for question_object in self.list_of_zofar_questions]
+        tmp_details_str = '\n'.join(tmp_details_list)
+        print(tmp_details_str)
+        print('\n\n')
+        raw_output = self.return_random_ao_json_str()
+        print(f'{len(raw_output)=}')
+        hexencoded_and_compressed = hexencode_and_compress(raw_output)
+        print(f'{len(hexencoded_and_compressed)=}')
+        print(f' would need: {math.ceil(len(hexencoded_and_compressed) / self.chars_per_db_variable)} zofar variables.')
+        compressed = compress(raw_output)
+        print(f'{len(compressed)=}')
+        print(f' would need: {math.ceil(len(compressed) / self.chars_per_db_variable)} zofar variables.')
+
+
+y = ZofarQuestionCollection(numer_of_episodes=10)
+
+y.add_question_object(ZofarQuestionSingleChoice(no_of_answer_options=10, list_of_variable_names=['var01']))
+y.add_question_object(ZofarQuestionSingleChoice(no_of_answer_options=5, list_of_variable_names=['var02']))
+y.add_question_object(ZofarQuestionSingleChoice(no_of_answer_options=3, list_of_variable_names=['var03']))
+y.add_question_object(ZofarQuestionSingleChoice(no_of_answer_options=10, list_of_variable_names=['var15']))
+y.add_question_object(ZofarQuestionSingleChoice(no_of_answer_options=5, list_of_variable_names=['var16']))
+y.add_question_object(ZofarQuestionSingleChoice(no_of_answer_options=3, list_of_variable_names=['var17']))
+
+y.add_question_object(ZofarQuestionMultipleChoice(list_of_variable_names=['var04', 'var05', 'var06']))
+y.add_question_object(ZofarQuestionMultipleChoice(list_of_variable_names=['var18', 'var19', 'var20']))
+y.add_question_object(
+    ZofarQuestionMultipleChoiceMatrix(no_of_answer_options=5, list_of_variable_names=['var07', 'var08', 'var09']))
+
+y.add_question_object(ZofarQuestionOpen(list_of_variable_names=['var10']))
+y.add_question_object(ZofarQuestionOpen(list_of_variable_names=['var11']))
+y.add_question_object(ZofarQuestionOpen(list_of_variable_names=['var12']))
+y.add_question_object(ZofarQuestionOpen(list_of_variable_names=['var13']))
+y.add_question_object(ZofarQuestionOpen(list_of_variable_names=['var14']))
+y.add_question_object(ZofarQuestionOpen(list_of_variable_names=['var15']))
+
+y.print_statistics()
+
+print('###')
+y.return_random_ao_json_str()
+
+for i in range(100):
+    t1 = return_lorem_text(2000)
+    print(t1)
+    print(len(t1))
+
+s = 'bc02685b7b226964223a302c227374617465223a226e6577222c22747970010d10536c6f74320d0f2c436f6c6f72223a2272656422052f08727444093374323032302d30332d30315430313a30303a30302e3030305a222c22656e64192518332d30322d32384225004473665f7661613031223a66616c73657d2c7b111300350d13002c05a600310577fea600fea60011a6005d'
+b = bytes.fromhex(s)
+
+t = snappy.decompress(b)
+r = t.decode('utf-8')
+print(r)
 
 test_dict = create_test_dict(number_of_keys=100, val_length=2000)
 
